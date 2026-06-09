044106f9ed8d2bdfa81604e7354f0b64aaba0422
Stephanie <stephanie_3141240114@utd.edu.mx>
2026-06-09 09:28:43 -0600

    Cambios en el app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const emailInput = document.getElementById('emailInput');
 const btnGenerar = document.getElementById('btnGenerar');
 const resultadoContenedor = document.getElementById('resultadoContenedor');
 const tokenOutput = document.getElementById('tokenOutput');
+const nombre_alumno = ("Soy Stephanie Pérez de los Ríos, mi programa terminó a las: ");
+const fecha = new Date();
 
 // 2. Escuchar el evento de clic en el botón
 btnGenerar.addEventListener('click', () => {
@@ -11,6 +13,7 @@ btnGenerar.addEventListener('click', () => {
     // Validación simple mediante el DOM
     if (correo === "" || !correo.includes('@')) {
         alert("Por favor, introduce un correo electrónico válido.");
+        console.error("No pusiste arroba, ponte listo")
         return;
     }
 
@@ -31,4 +34,5 @@ btnGenerar.addEventListener('click', () => {
     // Opcional: Cambiamos el estilo dinámicamente mediante el DOM para dar feedback visual
     tokenOutput.style.backgroundColor = "#e8f8f5"; 
     tokenOutput.style.borderLeftColor = "#2ecc71"; // Cambia a verde al completarse
+    console.info(nombre_alumno+fecha);
 });
